59e0f1183da268d280b6f0721a0d4c11eb610cf0
de-lia <d3liaayoko@gmail.com>
2023-01-25 05:43:06 +0100

    initial commit

--- MineGame/mine2/script.js
+++ MineGame/mine2/script.js
@@ -1,11 +1,11 @@
-const scoreEl = document.querySelector("#score");
+const scoreEl = document.querySelector("#score");   // returns the first element (#score in this case) that matches a CSS selector.
 
 const gameBoardEl = document.querySelector(".game-board");
 
 const messageElement = document.querySelector(".message");
 
-const minedSquare = Math.floor(Math.random() * 25) + 1;
-
+const minedSquare = Math.floor(Math.random() * 25) + 1;     //Math.random() creates a number between 0-0.9
+                                                            /* * 25 is multiplying random number by 25, which creates a number between 0-24. Math.floor() takes a decimal number and converts it to integer e.g 2.4 becomes 2. +1 is added to increase the range from 0-24 to 0-25 */
 console.log("Bad square is ==>", minedSquare);
 
 
@@ -15,30 +15,31 @@ let squaresLeft = 25;
 function checkMine(e, squareNumber){
     console.log("Clicked square is ==>", squareNumber);
 
-    if(squareNumber === minedSquare && squaresLeft === 1){
+    if(squareNumber === minedSquare && squaresLeft === 1){      // if the clicked square has been mined and it is the last,
         //Won!
-        e.target.classList.add("mined");
-        messageElement.textContent = "You win!";
+        e.target.classList.add("mined");        // adds class mined to the list
+        messageElement.textContent = "You win!";       // displays a message to the screen
         messageElement.getElementsByClassName.display = "block";
 
         score += 1;
 
         scoreEl.textContent = score.toString();
-    } else if(squareNumber === minedSquare && squaresLeft > 1){
+    } else if(squareNumber === minedSquare && squaresLeft > 1){     // else if the clicked square has been mined but there are more than 1 squares to be mined,
         //Lost
         e.target.classList.add("mined");
         messageElement.textContent = "You lose!"
         messageElement.style.display = "block";
-    } else{
+    } else{         // continue mining
         e.target.classList.add("safe");
     }
 
-    squaresLeft -= 1;
+    squaresLeft -= 1;       //reduces the number of mined squares by 1
 }
 
 for(let i=1; i<= 25; i++){
-    gameBoardEl.innerHTML += `<button id="square${i}" class="square" onclick="checkMine(event,${i})">${i}</button>`;
+    gameBoardEl.innerHTML += `<button id="square${i}" class="square" onclick="checkMine(event,${i})">${i}</button>`;    //display from 1-25 the values of the squares
 }
-function resetGame(){
+
+function resetGame(){   // refreshes the window when clicked
     window.location.reload("Reset");
 }
